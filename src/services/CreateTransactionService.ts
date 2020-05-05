460a11de00dbc78fad8e5a27dc3c2e2fbfdb8528
scard69 <vit.scard@gmail.com>
2020-05-04 22:55:36 -0300

    commit inicial

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transacation type is invalid');
+    }
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
